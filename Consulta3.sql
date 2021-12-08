a8d73a6b2053afa238af5152127a081e7c2d3d1a
Luisa Michelle Cruz <61128791+luisamich@users.noreply.github.com>
2021-12-08 17:09:07 -0600

    Add files via upload

--- Consulta3.sql
+++ Consulta3.sql
@@ -1,31 +1,51 @@
 -- Consulta3
-	---Saber que encargado regional cuenta con mas bonos
+	---Saber que encargado por fragmento cuenta con mas bonos
 	-- select BusinessEntityID from SalesPerson where Bonus= (select max(Bonus) from SalesPerson);
 
-alter procedure regional_bonos as
+GO
+ALTER procedure regional_bonos as
 begin
-	declare @servidor nvarchar(100);
-	declare @nom_bd nvarchar(100);
-	declare @nom_tabla nvarchar(100);
-	declare @sql nvarchar(1000);
-	declare @sql1 nvarchar(1000);
-	declare @sqlt nvarchar(1000);
-	declare @condicion varchar(200);
-	declare @i int = 0;
-	set @condicion ='Bonus = (select max(Bonus) from '
-	set  @nom_tabla='SalesPerson';
+	
+	DECLARE @sql nvarchar (500);
 
+
+	DECLARE @servidor varchar (100);
+	DECLARE @nom_bd varchar (100);
+	DECLARE @nom_tabla varchar (100);
+	DECLARE @columnas varchar (500); 
+
+	DECLARE @condicion varchar (100); 
+	DECLARE @i int = 0; 
+	DECLARE @tabla1 varchar (100);
+	DECLARE @tabla2 varchar (100);
+	SET @tabla1 = 'SalesPerson';
+	SET @tabla2 = 'LSERVER2.PersonInfo.dbo.Person'
+
+	--select Sales.SalesPerson.BusinessEntityID, Person.Person.FirstName, Person.Person.LastName
+	--from Sales.SalesPerson join Person.Person
+	--on Sales.SalesPerson.BusinessEntityID = Person.Person.BusinessEntityID
+	--where Bonus= (select max(Bonus) from Sales.SalesPerson);
+
+	SET @columnas = ' SalesPerson.BusinessEntityID, Person.FirstName, Person.LastName ';
+	
 	while @i<2
 	begin
-		set @i = @i+1;
-		select @servidor = servidor, @nom_bd = bd from diccionario_dist where id_fragmento = @i;
+		set @i = @i+1; 
+		select @servidor = servidor, @nom_bd = bd from diccionario_dist where  id_fragmento= @i; 
+
+
+		set @sql = 'select ' + @columnas + ' from ' 
+		+ '[' + @servidor +']'+'.'+ @nom_bd + '.dbo.' + @tabla1 +' join '
+		+ @tabla2
+		+ ' on ' +@tabla1+'.BusinessEntityID = Person.BusinessEntityID '
+		+ 'where Bonus = (select max(Bonus) from ' + '[' + @servidor +']'+'.'+ @nom_bd + '.dbo.' + @tabla1 +')'; 
 		
-			set @sql = 'select BusinessEntityID as "Encargado Regional" , TerritoryID as Territorio, Bonus as Bonos from ' + @servidor + '.' + @nom_bd + '.dbo.'+ @nom_tabla + ' '+  'where ' + @condicion + @servidor + '.' + @nom_bd + '.dbo.'+ @nom_tabla + ')';
-
-
 		
 		exec sp_executesql @sql
-		end 
+		print @sql;
+
+	end
+
 end
 
 exec regional_bonos;
